4f24284634b75202a6de442a47c86d2b6faaf9d0
DanLeech1 <D.Leech@2021.ljmu.ac.uk>
2022-01-25 13:53:51 +0000

    Tutorial2 Work

---
+++ week2.py
@@ -0,0 +1,25 @@
+x = 0
+list1 = ['bob', 'jim', 'dan']
+
+for i in range(len(list1)):
+
+    print(list1[x])
+    x = x + 1
+
+message1 = 'hello'
+message2 = 'hi'
+message3 = 'hola'
+
+print(message1, list1[0])
+print(message2, list1[1])
+print(message3, list1[2])
+
+
+list2 = ['Africa', 'North America', 'South America', 'Asia', 'Europe']
+
+print(list2)
+print(sorted(list2))
+list2.sort
+print(list2)
+list2.reverse
+print(list2)
